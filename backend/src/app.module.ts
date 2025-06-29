7f114e81582735a4c6fdb0c204c02ae525e99efb
soufiane <soufianeajaite2@gmail.com>
2025-06-29 19:32:06 +0100

    add ConfigModule and Database Setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true, // to be Removed in Prod
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! im soufiane';
+    return `Hello World! im soufiane this is database ${typeof process.env.DATABASE_PORT}`;
   }
 }
